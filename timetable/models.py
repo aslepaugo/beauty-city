ce5a664d00feb9d58462c41b9ea83ee5ac7e2ce1
VneTraffiqua <safe72again@gmail.com>
2022-12-07 10:00:45 -0800

    Create model User

--- timetable/models.py
+++ timetable/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    telegram_id = models.IntegerField(
+        verbose_name='Телеграм-ID клиента'
+    )
+    fullname = models.CharField(
+        max_length=100,
+        help_text='Введите имя и фамилию клиента',
+        verbose_name='Имя и фамилия клиента'
+    )
+    phone_number = models.PhoneNumberField(
+        'Номер клиента', blank=True, max_length=20
+    )
+
+    def __str__(self):
+        return f'{self.fullname}'
+
+    class Meta:
+        verbose_name = 'Клиенты'
