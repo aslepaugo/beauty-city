fb0f44e04e737097350e8defbbaf848cea51853b
VneTraffiqua <safe72again@gmail.com>
2022-12-07 23:34:08 -0800

    Added some models

--- timetable/models.py
+++ timetable/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class User(models.Model):
@@ -10,7 +11,7 @@ class User(models.Model):
         help_text='Введите имя и фамилию клиента',
         verbose_name='Имя и фамилия клиента'
     )
-    phone_number = models.PhoneNumberField(
+    phone_number = PhoneNumberField(
         'Номер клиента', blank=True, max_length=20
     )
 
@@ -18,4 +19,47 @@ class User(models.Model):
         return f'{self.fullname}'
 
     class Meta:
-        verbose_name = 'Клиенты'
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Мастера'
+
+
+class Master(models.Model):
+    fullname = models.CharField(
+        max_length=100,
+        help_text='Введите имя и фамилию мастера',
+        verbose_name='Имя и фамилия мастера'
+    )
+    telegram_id = models.IntegerField(
+        verbose_name='Телеграм-ID мастера'
+    )
+    speciality = models.CharField(
+        max_length=1000,
+        help_text='Введите специальность',
+        verbose_name='Специальность мастера',
+    )
+    saloon = models.CharField(
+        max_length=1000,
+        help_text='Салон в котором работает мастер',
+        verbose_name='Салон',
+    )
+
+    class Meta:
+        verbose_name = 'Мастер'
+        verbose_name_plural = 'Мастера'
+
+
+class Saloon(models.Model):
+    address = models.TextField(
+        'Адрес салона',
+        help_text='Адрес где находится салон')
+    masters = models.ForeignKey(
+        to=Master,
+        on_delete=models.CASCADE,
+        related_name='saloons',
+        verbose_name='Мастер',
+        null=True
+    )
+    
+    class Meta:
+        verbose_name = 'Салон'
+        verbose_name_plural = 'Салоны'
