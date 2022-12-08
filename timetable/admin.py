347ab273bf05e8cf66190059a482b89f04bbe9d8
VneTraffiqua <safe72again@gmail.com>
2022-12-07 23:33:44 -0800

    Added classes in admin panel

--- timetable/admin.py
+++ timetable/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
+from .models import User, Master, Saloon
 
-# Register your models here.
+
+@admin.register(User)
+class User(admin.ModelAdmin):
+    list_display = ['telegram_id', 'fullname']
+
+
+@admin.register(Master)
+class Master(admin.ModelAdmin):
+    list_display = (
+        'fullname', 'speciality', 'saloon'
+    )
+
+
+@admin.register(Saloon)
+class Saloon(admin.ModelAdmin):
+    list_display = ['address']
