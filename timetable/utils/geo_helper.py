2751429383fef67ab85670ea1554bb63c3505adf
Aleksy <aslepaugo@gmail.com>
2022-12-09 08:20:52 +0100

    Find closest  place to a location by lat lon

--- timetable/utils/geo_helper.py
+++ timetable/utils/geo_helper.py
@@ -1,6 +1,26 @@
 from geopy.geocoders import Nominatim
+from math import asin, cos, radians, sin, sqrt
+
+
+RADIUS_EARTH = 6378
 
 def get_lat_lon(address):
     geolocator = Nominatim(user_agent='BeautyCity Test')
     location = geolocator.geocode(address)
     return (location.latitude, location.longitude)
+
+
+def get_distance_between_lat_lon(*args):
+    lat1, lat2, long1, long2 = map(radians, args)
+    dist_lats = abs(lat2 - lat1) 
+    dist_longs = abs(long2 - long1) 
+    a = sin(dist_lats/2)**2 + cos(lat1) * cos(lat2) * sin(dist_longs/2)**2
+    c = asin(sqrt(a)) * 2
+    return c * RADIUS_EARTH
+
+
+def find_closest_lat_lon(location, places):
+    try:
+        return min(places, key=lambda place: get_distance_between_lat_lon(location['lat'],place['lat'],location['lon'],place['lon']))
+    except TypeError:
+        print('Not a list or not a number.')
